e95e8867a1127cc78d0c03fcf52df5486d90f4d1
Nicolas Verdonck <nverdonc@student.42.fr>
2015-12-16 16:05:06 +0100

    norme

--- src/sqweb-filter.php
+++ src/sqweb-filter.php
@@ -9,30 +9,26 @@ function filter_get_option( $value ) {
 }
 
 function filter_page_builder( $data = null, $object_id, $meta_key, $single ) {
-	if ( 'wpi_page_builder_shortcode' == $meta_key) {
+	if ( 'wpi_page_builder_shortcode' == $meta_key ) {
 		$meta_type = 'post';
-		$meta_cache = wp_cache_get($object_id, $meta_type . '_meta');
-		if ( !$meta_cache ) {
+		$meta_cache = wp_cache_get( $object_id, $meta_type . '_meta' );
+		if ( ! $meta_cache ) {
 			$meta_cache = update_meta_cache( $meta_type, array( $object_id ) );
-			$meta_cache = $meta_cache[$object_id];
+			$meta_cache = $meta_cache[ $object_id ];
 		}
 
 		if ( ! $meta_key ) {
 			return $meta_cache;
 		}
 
-		if ( isset($meta_cache[$meta_key]) ) {
-			$meta_cache[$meta_key]['0'] = str_replace('[wpi_ads_code_1]', '', $meta_cache[$meta_key]['0']);
-			if ( $single )
-				return maybe_unserialize( $meta_cache[$meta_key][0] );
-			else
-				return array_map('maybe_unserialize', $meta_cache[$meta_key]);
+		if ( isset( $meta_cache[ $meta_key ] ) ) {
+			$meta_cache[ $meta_key ]['0'] = str_replace( '[wpi_ads_code_1]', '', $meta_cache[ $meta_key ]['0'] );
+			if ( $single ) {
+				return maybe_unserialize( $meta_cache[ $meta_key ][0] ); } else { 				return array_map( 'maybe_unserialize', $meta_cache[ $meta_key ] ); }
 		}
 
-		if ($single)
-			return '';
-		else
-			return array();
+		if ( $single ) {
+			return ''; } else { 			return array(); }
 		return ('');
 	}
 	return null;
@@ -40,7 +36,7 @@ function filter_page_builder( $data = null, $object_id, $meta_key, $single ) {
 
 if ( sqweb_check_credentials( get_option( 'wsid' ) ) > 0 ) {
 
-/**
+	/**
  * Compatibility with major part of ads plugins
  */
 
@@ -57,7 +53,7 @@ if ( sqweb_check_credentials( get_option( 'wsid' ) ) > 0 ) {
 		add_filter( 'option_'.$value, 'filter_get_option', 100 );
 	}
 
-/**
+	/**
  * Compatibility with Page Builder ADS
  */
 
