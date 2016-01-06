75049e810e5e69e4e1b27770facfcf467fb0b7ac
Nicolas Verdonck <nverdonc@student.42.fr>
2016-01-06 10:58:11 +0100

    add others unitest, change timeout senddata, can timeout we don't care

--- src/tests/test-sample.php
+++ src/tests/test-sample.php
@@ -2,17 +2,29 @@
 
 class SampleTest extends WP_UnitTestCase {
 
-	function test_sample() {
-		// replace this with some actual testing code
-		$this->assertTrue( true );
-	}
-
-	function test_basic_sign_in() {
-		$result = sqweb_sign_in( 'sqwebtestrole1@gmail.com', 'testrole1' );
-		$this->assertNotEmpty( $result );
-
+	function test_basic_sign_in_false() {
 		$result = sqweb_sign_in( 'FalseEmail', 'FalsePassword' );
 		$this->assertEquals( $result, 0 );
+	}
+
+	function test_basic_sign_in_true() {
+		$result = sqweb_sign_in( 'sqwebtestrole1@gmail.com', 'testrole1' );
+		$this->assertNotEmpty( $result );
+	}
+
+	function test_basic_check_token_false() {
+		$result = sqweb_check_token( 'Je suis un faux token' );
+		$this->assertEquals( $result, 0 );
+	}
+
+	function test_basic_check_token_true() {
+		$result = sqweb_check_token( sqweb_sign_in( 'sqwebtestrole1@gmail.com', 'testrole1' ) );
+		$this->assertNotEquals( $result, 0 );
+	}
+
+	function test_basic_get_websites() {
+		$result = sqw_get_sites( sqweb_sign_in( 'sqwebtestrole1@gmail.com', 'testrole1' ) );
+		$this->assertNotEquals( $result, 0 );
 	}
 }
 
