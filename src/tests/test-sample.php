bfb16e1d525d3c85280d94a91b8db5f2059d2903
Nicolas Verdonck <nverdonc@student.42.fr>
2016-01-06 16:55:27 +0100

    correct test

--- src/tests/test-sample.php
+++ src/tests/test-sample.php
@@ -8,7 +8,7 @@ class SampleTest extends WP_UnitTestCase {
 	}
 
 	function test_basic_sign_in_true() {
-		$result = sqweb_sign_in( 'sqwebtestrole1@gmail.com', 'testrole1' );
+		$result = sqweb_sign_in( 'test-wp@sqweb.com', 'test-wp:)' );
 		$this->assertNotEmpty( $result );
 	}
 
@@ -18,12 +18,12 @@ class SampleTest extends WP_UnitTestCase {
 	}
 
 	function test_basic_check_token_true() {
-		$result = sqweb_check_token( sqweb_sign_in( 'sqwebtestrole1@gmail.com', 'testrole1' ) );
+		$result = sqweb_check_token( sqweb_sign_in( 'test-wp@sqweb.com', 'test-wp:)' ) );
 		$this->assertNotEquals( $result, 0 );
 	}
 
 	function test_basic_get_websites() {
-		$result = sqw_get_sites( sqweb_sign_in( 'sqwebtestrole1@gmail.com', 'testrole1' ) );
+		$result = sqw_get_sites( sqweb_sign_in( 'test-wp@sqweb.com', 'test-wp:)' ) );
 		$this->assertNotEquals( $result, 0 );
 	}
 }
