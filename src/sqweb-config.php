911e46fda2dc1c4714d3b2dee3e555a13781ae89
Nicolas Verdonck <nicolas@sqweb.com>
2016-06-07 17:52:48 +0200

    Improve Data stockage for remove usage of late init on Wordpress super cache

---
+++ src/sqweb-config.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+	'filter.ads' => 'a:1:{s:32:"aa03606c6648deed944ad1494d3dd014";s:3:"ADS";}',
+	'filter.text' => 'a:1:{s:32:"aa03606c6648deed944ad1494d3dd014";s:7:"CONTENT";}',
+);
--- src/sqweb-wsc-filter.php
+++ src/sqweb-wsc-filter.php
@@ -6,8 +6,8 @@
 class SQweb_filter
 {
 	private static $_instance = null;
-	public $_ads;
-	public $_text;
+	private $_ads;
+	private $_text;
 	private $_data_set = false;
 	private $_w3tc;
 
@@ -22,35 +22,6 @@ class SQweb_filter
 			add_cacheaction( 'wpsc_cachedata', array( $this, 'display_ads_with_wpsc' ) );
 		}
 		$this->get_w3tc_settings();
-	}
-
-	public function get_w3tc_settings() {
-		$wp_master_path = WP_CONTENT_DIR . '/w3tc-config/master.php';
-		/** Check if W3TC is enabled */
-		if ( file_exists( $wp_master_path ) ) {
-			$this->_w3tc = include( $wp_master_path );
-			if ( $this->_w3tc['pgcache.enabled'] ) {
-				if ( ! $this->_w3tc['pgcache.late_init'] ) {
-					/** Active Late Init */
-					$file = file_get_contents( $wp_master_path );
-					$file = str_replace( '\'pgcache.late_init\' => false', '\'pgcache.late_init\' => true', $file );
-					file_put_contents( $wp_master_path, $file );
-				}
-				if ( ! defined( 'W3TC_DYNAMIC_SECURITY' ) ) {
-					/** Define W3TC DYNAMIC SECURITY */
-					$wp_cache_config_file = ABSPATH . '/wp-config.php';
-					$file = file_get_contents( $wp_cache_config_file );
-					$file = str_replace( '/** Enable W3 Total Cache */', '/** Dynamic content for SQweb */
-define( \'W3TC_DYNAMIC_SECURITY\', \'' . md5( rand() ) . '\');
-
-/** Enable W3 Total Cache */', $file );
-					file_put_contents( $wp_cache_config_file, $file );
-				}
-				if ( $this->_w3tc['pgcache.engine'] === 'file' && $this->_w3tc['browsercache.enabled'] ) {
-					add_action( 'admin_notices', array( $this, 'notice_browser_cache' ) );
-				}
-			}
-		}
 	}
 
 	public static function get_instance() {
@@ -71,7 +42,15 @@ define( \'W3TC_DYNAMIC_SECURITY\', \'' . md5( rand() ) . '\');
 	public function notice_browser_cache() {
 	?>
 	<div class="notice notice-error is-dismissible">
-		<p><?php _e( '<b>SQweb notice : </b>Browser cache from W3TC is enabled, is not compatible with page cache method : Disk: Basic, disabled it or switch on Disk: enhanced.', 'sqweb' ); ?></p>
+		<p><?php _e( '<b>SQweb notice : </b>Browser cache from W3TC is enabled, is not compatible with page cache method : Disk: Basic, please disabled it.', 'sqweb' ); ?></p>
+	</div>
+	<?php
+	}
+
+	public function notice_engine() {
+	?>
+	<div class="notice notice-error is-dismissible">
+		<p><?php _e( '<b>SQweb notice : </b>Dynamic content is not available in Disk: enhanced mod, please switch on Disk: basic.', 'sqweb' ); ?></p>
 	</div>
 	<?php
 	}
@@ -83,19 +62,53 @@ define( \'W3TC_DYNAMIC_SECURITY\', \'' . md5( rand() ) . '\');
 			$wp_cache_mfunc_enabled = 1;
 			$file = file_get_contents( $wp_cache_config_file );
 			$file = str_replace( '$wp_cache_mfunc_enabled = 0; //Added by WP-Cache Manager', '$wp_cache_mfunc_enabled = 1; //Edited by SQweb', $file );
+			/** Automatic activate late init, deprecated since 2.1.1
 			$file = str_replace( '$wp_super_cache_late_init = 0; //Added by WP-Cache Manager', '$wp_super_cache_late_init = 1; //Edited by SQweb', $file );
+			*/
 			file_put_contents( $wp_cache_config_file, $file );
 		} else {
 			add_action( 'admin_notices', array( $this, 'notice_mod_rewrite' ) );
 		}
 	}
 
+	public function get_w3tc_settings() {
+		$wp_master_path = WP_CONTENT_DIR . '/w3tc-config/master.php';
+		/** Check if W3TC is enabled */
+		if ( file_exists( $wp_master_path ) ) {
+			$this->_w3tc = include( $wp_master_path );
+			if ( $this->_w3tc['pgcache.enabled'] ) {
+				if ( ! $this->_w3tc['pgcache.late_init'] ) {
+					/* Active Late Init */
+					$file = file_get_contents( $wp_master_path );
+					$file = str_replace( '\'pgcache.late_init\' => false', '\'pgcache.late_init\' => true', $file );
+					file_put_contents( $wp_master_path, $file );
+				}
+				if ( ! defined( 'W3TC_DYNAMIC_SECURITY' ) ) {
+					/** Define W3TC DYNAMIC SECURITY */
+					$wp_cache_config_file = ABSPATH . '/wp-config.php';
+					$file = file_get_contents( $wp_cache_config_file );
+					$file = str_replace( '/** Enable W3 Total Cache */', '/** Dynamic content for SQweb */
+define( \'W3TC_DYNAMIC_SECURITY\', \'' . md5( rand() ) . '\');
+
+/** Enable W3 Total Cache */', $file );
+					file_put_contents( $wp_cache_config_file, $file );
+				}
+				if (  $this->_w3tc['browsercache.enabled'] ) {
+					add_action( 'admin_notices', array( $this, 'notice_browser_cache' ) );
+				}
+				if ( 'file' !== $this->_w3tc['pgcache.engine'] ) {
+					add_action( 'admin_notices', array( $this, 'notice_engine' ) );
+				}
+			}
+		}
+	}
+
 	public function set_data() {
 		if ( ! $this->_data_set ) {
-			$this->_ads = unserialize( get_option( 'sqw_ads' ) );
-			$this->_text = unserialize( get_option( ' sqw_text' ) );
-			$this->_ads = $this->_ads ? $this->_ads : array();
-			$this->_text = $this->_text ? $this->_text : array();
+			$sqweb_config_path = WP_PLUGIN_DIR . '/sqweb/sqweb-config.php';
+			$sqweb_config = include( $sqweb_config_path );
+			$this->_ads = unserialize( $sqweb_config['filter.ads'] );
+			$this->_text = unserialize( $sqweb_config['filter.text'] );
 			$this->_data_set = true;
 		}
 	}
@@ -104,11 +117,17 @@ define( \'W3TC_DYNAMIC_SECURITY\', \'' . md5( rand() ) . '\');
 
 		$this->set_data();
 		if ( ! in_array( $ads, $this->_ads ) ) {
+			$sqweb_config_path = WP_PLUGIN_DIR . '/sqweb/sqweb-config.php';
 			$key = md5( rand() );
 			$this->_ads[ $key ] = $ads;
 			$this->_text[ $key ] = $text;
-			update_option( 'sqw_ads', serialize( $this->_ads ) );
-			update_option( 'sqw_text', serialize( $this->_text ) );
+			$content = '<?php
+
+return array(
+	\'filter.ads\' => \'' . serialize( $this->_ads ) . '\',
+	\'filter.text\' => \'' . serialize( $this->_text ) . '\',
+);';
+			file_put_contents( $sqweb_config_path, $content );
 		} else {
 			$key = array_search( $ads, $this->_ads );
 		}
@@ -118,16 +137,18 @@ define( \'W3TC_DYNAMIC_SECURITY\', \'' . md5( rand() ) . '\');
 	private function display_ads( $key ) {
 
 		global $cache_enabled, $super_cache_enabled;
-		
+
 		if ( $cache_enabled && $super_cache_enabled ) {
 			echo $key;
-		} else if ( $this->_w3tc['pgcache.enabled'] ) {
+		} elseif ( $this->_w3tc['pgcache.enabled'] ) {
 			?>
 			<!--mfunc <?php echo W3TC_DYNAMIC_SECURITY; ?> -->
+				$sqweb_config = include( '<?php echo WP_PLUGIN_DIR; ?>/sqweb/sqweb-config.php' );
+				include_once( '<?php echo WP_PLUGIN_DIR; ?>/sqweb/functions.php');
 				if ( sqweb_check_credentials() > 0 ) {
-					echo unserialize( get_option( 'sqw_text' ) )['<?php echo $key; ?>'];
+					echo unserialize( $sqweb_config['filter.text'] )['<?php echo $key; ?>'];
 				} else {
-					echo unserialize( get_option( 'sqw_ads' ) )['<?php echo $key; ?>'];
+					echo unserialize( $sqweb_config['filter.ads'] )['<?php echo $key; ?>'];
 				}
 			<!--/mfunc <?php echo W3TC_DYNAMIC_SECURITY; ?> -->
 			<?php
