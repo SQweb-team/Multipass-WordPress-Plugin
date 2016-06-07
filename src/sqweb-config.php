b6471c29b302a745701fa0737842216c21433c76
Nicolas Verdonck <nicolas@sqweb.com>
2016-06-07 17:55:51 +0200

    reset config file

--- src/sqweb-config.php
+++ src/sqweb-config.php
@@ -1,6 +1,6 @@
 <?php
 
 return array(
-	'filter.ads' => 'a:1:{s:32:"aa03606c6648deed944ad1494d3dd014";s:3:"ADS";}',
-	'filter.text' => 'a:1:{s:32:"aa03606c6648deed944ad1494d3dd014";s:7:"CONTENT";}',
+	'filter.ads' => 'a:0:{}',
+	'filter.text' => 'a:0:{}',
 );
