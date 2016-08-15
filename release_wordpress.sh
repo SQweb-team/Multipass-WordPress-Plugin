aab59508bed2ced6d7a75497f8017532020f9622
Pierre Lavaux <pierre.lvx@gmail.com>
2016-08-15 17:48:20 +0200

    Release handler : misc fixes + verbose output.

--- release_wordpress.sh
+++ release_wordpress.sh
@@ -11,29 +11,37 @@ DIR_DIST_SVN="../sqweb-wordpress-plugin-svn/sqweb"
 GIT_TAG=$(git describe --abbrev=0 --tags)
 GIT_TAG_SHORT=$(git describe --abbrev=0 --tags | cut -d v -f 2)
 
-# Creating a release and extracting it
-gulp && unzip sqweb-wordpress-plugin.zip
+# Make sure that node dependencies are installed.
+npm install
 
-# Cleaning up the trunk
-rm -rf $DIR_DIST_SVN/trunk/* && svn delete trunk/*
+echo "Creating a release and extracting it..."
+gulp && unzip ./dist/sqweb-wordpress-plugin.zip -d $DIR_DIST
 
-# Copying the release to the trunk
+echo "Cleaning up the trunk..."
+rm -rf $DIR_DIST_SVN/trunk/* && svn delete $DIR_DIST_SVN/trunk
+
+echo "Copying the release to the trunk..."
 cp -R $DIR_DIST $DIR_DIST_SVN/trunk
 
-# Create a release tag in svn
+echo "Create a release tag in svn..."
 mkdir $DIR_DIST_SVN/tags/$GIT_TAG_SHORT
 
-# Copying the release to the svn tag
+echo "Copying the release to the svn tag..."
 cp -R $DIR_DIST $DIR_DIST_SVN/tags/$GIT_TAG_SHORT
 
-# Cleaning up the release sources
+echo "Cleaning up the release sources..."
 rm -rf ./dist/*
 
-# Moving into the svn folder
+echo "Moving into the svn folder..."
 cd ../sqweb-wordpress-plugin-svn/sqweb
 
-# Checking out to svn
+# Confirming we're in the proper directory
+pwd
+
+echo "Checking out to svn..."
 svn add tags/$GIT_TAG_SHORT && svn add trunk/*
 
-# Pushing to SVN
+echo "Pushing to SVN..."
 svn ci -m "$GIT_TAG"
+
+echo "Done!"
