ec00bd5adf45b7ffb5e92d4710adabf9769467d6
Pierre Lavaux <pierre@lavaux.lv>
2017-05-10 12:44:18 +0200

    Coding Style.

--- src/diagnostic.php
+++ src/diagnostic.php
@@ -6,23 +6,23 @@ if ( ! empty( $_GET['type'] ) && 'diagnostic' == $_GET['type'] ) {
 	$plugins = get_plugins();
 	$infos = array();
 	$infos['wordpress'] = array(
-		'name' => get_bloginfo('name'),
-		'version' => get_bloginfo('version'),
-		'wpurl' => get_bloginfo('wpurl'),
-		'url' => get_bloginfo('url'),
-		'admin_email' => get_bloginfo('admin_email')
+		'name' => get_bloginfo( 'name' ),
+		'version' => get_bloginfo( 'version' ),
+		'wpurl' => get_bloginfo( 'wpurl' ),
+		'url' => get_bloginfo( 'url' ),
+		'admin_email' => get_bloginfo( 'admin_email' ),
 	);
 	foreach ( $plugins as $value ) {
 		$infos['plugins'][$value['Name']] = array(
 			'Version' => $value['Version'],
-			'PluginURI' => $value['PluginURI']
+			'PluginURI' => $value['PluginURI'],
 		);
 	}
 	$message = '';
-	$headers = array('Content-Type: text/html; charset=UTF-8');
+	$headers = array( 'Content-Type: text/html; charset=UTF-8' );
 	foreach ( $infos as $key => $info ) {
 		if ( 'wordpress' == $key ) {
-			$message .= 'About wordpress:<br><br>name => ' . $info['name'] . '<br>version => ' . $info['version'];
+			$message .= 'About WordPress:<br><br>name => ' . $info['name'] . '<br>version => ' . $info['version'];
 			$message .= '<br>wpurl => ' . $info['wpurl'] . '<br>url => ' . $info['url'] . '<br>admin_email => ' . $info['admin_email'];
 		} else {
 			$message .= '<br><br>Plugins: <br><br>';
@@ -38,5 +38,3 @@ if ( ! empty( $_GET['type'] ) && 'diagnostic' == $_GET['type'] ) {
 	wp_redirect( remove_query_arg( 'type' ) );
 	exit;
 } // End if().
-
-?>
