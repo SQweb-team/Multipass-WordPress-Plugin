0efbb4ad1b5202184a60344604bafe5feea9899b
Pierre Lavaux <pierre@lavaux.lv>
2017-05-12 10:45:11 +0200

    Upgraded minimum PHP version.

--- src/sqweb.php
+++ src/sqweb.php
@@ -79,7 +79,7 @@ register_deactivation_hook( __FILE__, 'sqw_deactivation' );
  */
 function check() {
 
-	if ( version_compare( phpversion(), '5.2.17', '<' ) ) {
+	if ( version_compare( phpversion(), '5.3.29', '<' ) ) {
 		deactivate_plugins( plugin_basename( __FILE__ ) );
 		echo '<div class="error"><p><b>Error : </b>You are using an unsupported version of PHP (' . phpversion() . '). The SQweb plugin cannot be activated.</p></div>';
 	}
