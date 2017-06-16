db85545ed2d2893619bc93f807940134d5d545d9
Pierre Lavaux <PierreLvx@users.noreply.github.com>
2017-06-16 12:00:39 +0200

    v2.7.1 (#103)
    * Added a shortcode to enhance ads filtering in themes.
    
    * Travis

--- src/shortcode-function.php
+++ src/shortcode-function.php
@@ -12,4 +12,20 @@ function sqweb_button_short_code( $atts = array(), $content = null, $tag = '' ) 
 	echo '<div class="sqweb-button' . ( 'normal' !== $wporg_atts['type'] ? ' multipass-' . $wporg_atts['type'] : '' ) . '"></div>';
 }
 
+function sqweb_add_filter_short_code( $atts = array(), $content = null, $tag = '' ) {
+
+	$wporg_atts = shortcode_atts( array(
+		'ads' => 'ad',
+		'premium' => '',
+	), $atts, $tag);
+
+	if ( apply_filters( 'sqw_check_credentials', get_option( 'wsid' ) ) ) {
+		echo $wporg_atts['premium'];
+	} else {
+		echo $wporg_atts['ads'];
+	}
+}
+
 add_shortcode( 'sqweb_button', 'sqweb_button_short_code' );
+
+add_shortcode( 'sqweb_add_filter', 'sqweb_add_filter_short_code' );
