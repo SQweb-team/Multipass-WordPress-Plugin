ca5c56646690c45bfe51cee03df1c44ee9323586
Pierre Lavaux <pierre@lavaux.lv>
2017-06-16 12:03:40 +0200

    API endpoint ; strict equal.

--- src/config.php
+++ src/config.php
@@ -15,11 +15,11 @@ global $wpdb;
  * @SuppressWarnings(PHPMD.Superglobals)
  */
 function sqw_site_url() {
-	$protocol = ( ! empty( $_SERVER['HTTPS'] ) && 'off' !== $_SERVER['HTTPS'] || 443 == $_SERVER['SERVER_PORT'] ) ? 'https://' : 'http://';
+	$protocol = ( ! empty( $_SERVER['HTTPS'] ) && 'off' !== $_SERVER['HTTPS'] || 443 === $_SERVER['SERVER_PORT'] ) ? 'https://' : 'http://';
 	return $protocol . $_SERVER['HTTP_HOST'];
 }
 
 // Self-Explanatory
-define( 'SQW_ENDPOINT', 'https://api.sqweb.com/' );
+define( 'SQW_ENDPOINT', 'https://api.multipass.net/' );
 define( 'DEBUG_MODE', 0 );
 define( 'SQW_VERSION', '2.7.1' );
