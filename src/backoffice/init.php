761005ee239613c8d6d776540f01580b6032174c
Mathieu Darrigade <mathieu@sqweb.com>
2017-10-25 12:18:08 +0200

    norme travis

--- src/backoffice/init.php
+++ src/backoffice/init.php
@@ -1,9 +1,9 @@
 <?php
-	$sqw_token = (get_option( 'sqw_token' ) != false) ? get_option( 'sqw_token' ) : '';
-	$wsid = (get_option( 'wsid' ) != false) ? get_option( 'wsid' ) : 0;
-	$flogin = (get_option( 'flogin' ) != false) ? get_option( 'flogin' ) : '';
-	$flogout = (get_option( 'flogout' ) != false) ? get_option( 'flogout' ) : '';
-	$fmes = (get_option( 'fmes' ) != false) ? get_option( 'fmes' ) : '';
-	$btheme = (get_option( 'btheme' ) != false) ? get_option( 'btheme' ) : 'blue';
-	$lang = (get_option( 'sqw_lang' ) != false) ? get_option( 'sqw_lang' ) : 'en';
-	$targeting = (get_option( 'targets' ) != false) ? get_option( 'targets' ) : 'false';
+	$sqw_token = ( get_option( 'sqw_token' ) != false ) ? get_option( 'sqw_token' ) : '';
+	$wsid      = ( get_option( 'wsid' ) != false ) ? get_option( 'wsid' ) : 0;
+	$flogin    = ( get_option( 'flogin' ) != false ) ? get_option( 'flogin' ) : '';
+	$flogout   = ( get_option( 'flogout' ) != false ) ? get_option( 'flogout' ) : '';
+	$fmes      = ( get_option( 'fmes' ) != false ) ? get_option( 'fmes' ) : '';
+	$btheme    = ( get_option( 'btheme' ) != false ) ? get_option( 'btheme' ) : 'blue';
+	$lang      = ( get_option( 'sqw_lang' ) != false ) ? get_option( 'sqw_lang' ) : 'en';
+	$targeting = ( get_option( 'targets' ) != false ) ? get_option( 'targets' ) : 'false';
